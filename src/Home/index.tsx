c3b9f6f99bc55ae9b5db464c2f33bb2dd8cfda97
douglas <38635101+dougdbp@users.noreply.github.com>
2023-06-25 17:10:59 -0300

    feat: Leitura de sorriso e piscadinha aplicados

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,11 +1,66 @@
-import { View } from 'react-native';
-import { Camera } from 'expo-camera';
+import { useEffect, useState } from 'react';
+import { ImageSourcePropType, View } from 'react-native';
+import { Camera, CameraType, FaceDetectionResult } from 'expo-camera';
+import * as FaceDetector from 'expo-face-detector';
+import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
+import neutralImg from '../assets/neutral.png';
+import smilingImg from '../assets/smiling.png';
+import winkingImg from '../assets/winking.png';
 
 import { styles } from './styles';
-import { useEffect } from 'react';
 
 export function Home() {
+    const [faceDetected, setFaceDetected] = useState(false);
     const [permission, requestPermission] = Camera.useCameraPermissions();
+    const [emoji, setEmoji] = useState<ImageSourcePropType>(neutralImg);
+
+    const faceValues = useSharedValue({
+        width: 0,
+        height: 0,
+        x: 0,
+        y: 0
+    });
+
+    function handleFacesDetected({ faces }: FaceDetectionResult){
+        const face = faces[0] as any;
+
+        if(face){
+            const { size, origin } = face.bounds;
+            
+            faceValues.value = {
+                width: size.width,
+                height: size.height,
+                x: origin.x,
+                y: origin.y
+            }
+            
+            setFaceDetected(true);
+
+            if(face.smilingProbability > 0.5) {
+                setEmoji(smilingImg);
+            }
+            else if(face.leftEyeOpenProbability < 0.5 && face.rightEyeOpenProbability > 0.5){
+                setEmoji(winkingImg)
+            }
+            else{
+                setEmoji(neutralImg);
+            };
+
+        }else{
+            setFaceDetected(false);
+        }
+    }
+
+    const animatedStyle = useAnimatedStyle(() => ({
+        position: 'absolute',
+        zIndex: 1,
+        width: faceValues.value.width,
+        height: faceValues.value.height,
+        transform: [
+            { translateX: faceValues.value.x},
+            { translateY: faceValues.value.y},
+        ],
+    }));
 
     useEffect(() => {
         requestPermission()
@@ -17,7 +72,26 @@ export function Home() {
 
   return (
     <View style={styles.container}>
-        <Camera style={styles.camera} />
+        { 
+            faceDetected && 
+            <Animated.Image 
+                style={animatedStyle} 
+                source={emoji}
+            /> 
+        }
+        <Camera 
+        style={styles.camera}
+        type={CameraType.front
+        }
+        onFacesDetected={handleFacesDetected}
+        faceDetectorSettings={{
+            mode: FaceDetector.FaceDetectorMode.fast,
+            detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+            runClassifications: FaceDetector.FaceDetectorClassifications.all,
+            minDetectionInterval: 100,
+            tracking: true,
+        }}
+        />
     </View>
   );
 }
